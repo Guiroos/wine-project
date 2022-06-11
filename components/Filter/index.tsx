2bed13288036f9c818f2e3f5f7050dcf20fc3dfd
guiroos <guilherme.roosr@gmail.com>
2022-06-11 13:21:25 -0300

    feat: created Filter Component

---
+++ components/Filter/index.tsx
@@ -0,0 +1,37 @@
+import {FilterList} from './style';
+
+export default function Filter() {
+  return (
+    <div>
+      <h1>Refine sua Busca</h1>
+      <FilterList>
+        <p>Por preço</p>
+        <li>
+          <label>
+            <input type="radio" value="40" name="filter" /> Até R$40
+          </label>
+        </li>
+        <li>
+          <label>
+            <input type="radio" value="40" name="filter" /> R$40 A R$60
+          </label>
+        </li>
+        <li>
+          <label>
+            <input type="radio" value="40" name="filter" /> R$100 A R$200
+          </label>
+        </li>
+        <li>
+          <label>
+            <input type="radio" value="40" name="filter" /> R$200 A R$500
+          </label>
+        </li>
+        <li>
+          <label>
+            <input type="radio" value="40" name="filter" /> Acima de R$500
+          </label>
+        </li>
+      </FilterList>
+    </div>
+  );
+}
