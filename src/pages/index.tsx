483f0b91d3d319a7bcfb29216bc3cdcc02498d45
guiroos <guilherme.roosr@gmail.com>
2022-06-13 06:47:29 -0300

    feat: created Home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar/index";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Favor direcionar a aba Loja</h1>
+    </div>
+  );
+};
+
+export default Home;
