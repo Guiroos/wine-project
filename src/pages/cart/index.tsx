29335799d65b53dce92459084fb6a7e81f67e2a1
guiroos <guilherme.roosr@gmail.com>
2022-06-13 06:41:29 -0300

    feat: created Cart page

---
+++ src/pages/cart/index.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import CartItemsCard from "../../components/CartItemsCard";
+import Navbar from "../../components/Navbar";
+import { getItemLocalStorage } from "../../utils/localStorage";
+
+const Cart: React.FC = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    const cartItems = getItemLocalStorage("cart");
+    setCartItems(cartItems);
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <div>
+        {cartItems ? (
+          cartItems.map((cartItem) => (
+            <CartItemsCard key={cartItem.id} cartItem={cartItem} />
+          ))
+        ) : (
+          <div>Não há itens no carrinho</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
