fd0075a17d09392238098a497e94d3cafa4bc8f1
guiroos <guilherme.roosr@gmail.com>
2022-06-16 05:00:02 -0300

    feat: created producers club page

---
+++ src/pages/producers/index.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/Navbar/index";
+
+const Producers: React.FC = () => {
+  return (
+    <div id="producers-page">
+      <Navbar />
+      <h1>Produtores (a desenvolver)</h1>
+    </div>
+  );
+};
+
+export default Producers;
